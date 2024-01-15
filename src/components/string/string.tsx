e30eff6838231f587e7f50f5a17df7d89c9552b8
Kochkina Ekaterina <Gudrum@yandex.ru>
2024-01-15 16:03:26 +0100

    feat: создать заполнение стейта по таймингу в   string.tsx// Create state filling by timing in string.tsx

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
 import {Circle} from "../ui/circle/circle";
 import {Input} from "../ui/input/input";
 import {Button} from "../ui/button/button";
@@ -7,39 +7,73 @@ import {ElementStates} from "../../types/element-states";
 
 export const StringComponent: React.FC = () => {
 
-  const [letters, setLetters] = useState<Array<{ id: number, name: string}> | null>(null)
+  const [letters, setLetters] = useState<Array<{ id: number, name: string }> | null>(null)
   const [value, setValue] = useState<string>("")
+  const [reverseLetters, setReverseLetters] = useState<Array<{ id: number, name: string }> | null>(null)
+const [isLoader, setIsLoader] = useState(false)
 
-  let isLoader = false
 
+  function reverseArray(inputArray: Array<{ id: number, name: string }>): Array<{ id: number, name: string }> {
+    return [...inputArray].reverse();
+  }
 
   function handlerOnClick() {
+    setReverseLetters(null)
+
+
     if (value) {
+      setIsLoader(true)
+
       const test1 = value.split('')
-      const test2 = test1.map( item => ({id:55, name: item}))
+      const test2 = test1.map(item => ({id: 55, name: item}))
       setLetters(test2)
       setValue("")
+
     }
   }
 
+  React.useEffect(() => {
+    let reverseTimeoutId: NodeJS.Timeout;
+    let lettersTimeoutId: NodeJS.Timeout;
 
-  function handlerOnChange(e:React.ChangeEvent<HTMLInputElement>) {
+    if (letters) {
+      reverseTimeoutId = setTimeout(() => {
+        setReverseLetters(reverseArray(letters!));
+      }, 3000);
+
+      lettersTimeoutId = setTimeout(() => {
+        setLetters(null);
+
+        setIsLoader(false)
+      }, 6000);
+    }
+
+    return () => {
+      clearTimeout(reverseTimeoutId);
+      clearTimeout(lettersTimeoutId);
+    };
+
+
+  }, [letters]);
+
+  function handlerOnChange(e: React.ChangeEvent<HTMLInputElement>) {
     setValue(e.target.value)
   }
 
 
-  const result = (array: Array<{ id: number, name: string}>): JSX.Element => {
+  const result = (array: Array<{ id: number, name: string }>): JSX.Element => {
     return (
-      <>
-        {array.map((item) => <Circle state={ElementStates.Default} letter={item.name} key={item.id} />)}
-      </>)
+      <div className="container-result">
+        {array.map((item) => <Circle state={ElementStates.Default} letter={item.name} key={item.id}/>)}
+      </div>)
   }
 
-
-
-
-
-
+  const result2 = (array: Array<{ id: number, name: string }>): JSX.Element => {
+    return (
+      <div className="container-result">
+        {array.map((item) => <Circle state={ElementStates.Changing} letter={item.name} key={item.id}/>)}
+      </div>)
+  }
 
 
   return (
@@ -48,7 +82,7 @@ export const StringComponent: React.FC = () => {
         <Input maxLength={11} isLimitText={true} onChange={handlerOnChange} value={value}/>
         <Button onClick={handlerOnClick} text={"Развернуть"} isLoader={isLoader} disabled={!value}/>
       </div>
-      <div className="container-result">
+      <div className="container-result-column">
 
 
         {/*<Circle state={ElementStates.Changing} letter={"lett"} head={"head"} index={8} isSmall={false}
@@ -61,9 +95,10 @@ export const StringComponent: React.FC = () => {
 
 
         {letters && result(letters)}
+        {reverseLetters && result2(reverseLetters)}
 
       </div>
-     
+
     </SolutionLayout>
   );
 };
